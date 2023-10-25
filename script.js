5eece8a389f704e7bce94c1ce369979d4b9710c0
Jessica Nguyen <89553284+Jnguye84@users.noreply.github.com>
2023-10-25 17:54:02 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+/*<script>
+    // Get a reference to the "Projects" heading using its id
+    var projectsHeading = document.getElementById("projects-heading");
+
+    // Add a click event listener to the "Projects" heading
+    projectsHeading.addEventListener("click", function() {
+        // Redirect the user to the "project_page.html" page
+        window.location.href = "project_page.html";
+    });
+</script>*/
